375d18007f0697c84fa1558fa8e3d41f9fb37443
章宦港 <huangangzhang@gmail.com>
2019-07-08 10:13:10 +0800

    判断是不是云闪付付款码

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict'
 
 class PayCode {
+  static isNumber (num) {
+    return typeof num === 'number'
+  }
   /**
    * 是不是微信支付付款码
    * https://pay.weixin.qq.com/wiki/doc/api/micropay_sl.php?chapter=5_1
@@ -8,7 +11,7 @@ class PayCode {
    * @param {*} code
    */
   static isWeixin (code) {
-    if (typeof code !== 'number') {
+    if (!PayCode.isNumber(code)) {
       return false
     }
     let top2 = code.toString().slice(0, 2) * 1
@@ -22,7 +25,7 @@ class PayCode {
    * @param {*} code
    */
   static isAlipay (code) {
-    if (typeof code !== 'number') {
+    if (!PayCode.isNumber(code)) {
       return false
     }
     code = code.toString()
@@ -35,10 +38,19 @@ class PayCode {
 
   /**
    * 是不是云闪付付款码
+   * 62 前缀+2位(主产品标识)+15位随机数
    * @param {*} code
    */
   static isUnionpay (code) {
-
+    if (!PayCode.isNumber(code)) {
+      return false
+    }
+    code = code.toString()
+    if (!(code.length !== 19)) {
+      return false
+    }
+    let top2 = code.slice(0, 2) * 1
+    return top2 === 62
   }
 }
 
